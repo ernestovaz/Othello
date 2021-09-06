62b9a813b7e07adfa8a95826099c35c8c92b032b
ernesto <ernesto.oliveira@inf.ufrgs.br>
2021-09-06 17:24:37 -0300

    Criada base do arquivo principal

---
+++ agent.py
@@ -0,0 +1,8 @@
+def make_move(the_board, color):
+    """
+    Retorna jogada de Othello
+    the_board: objeto board.Board com estado do jogo
+    color:  caractere indicando cor que será jogada ('B' ou 'W')
+    retorna tupla (int,int) com x,y da jogada
+    """
+    return None
