f31224830c3d40fb3fa822708858a182ec7e188e
ricco <ricco@localhost.localdomain>
2021-09-17 17:55:24 -0300

    added depth to the alpha-beta pruning.

--- alfa/agent.py
+++ alfa/agent.py
@@ -9,9 +9,10 @@ WHITE = 'W'
 EMPTY = '.'
 MAX_SCORE = 1000
 MIN_SCORE = -1000
+DEPTH = 3
 
 
-def eval_func(state : Board,color : str): #retorna qt. peças da cor
+def eval_func(state : Board, color : str): #retorna qt. peças da cor
     s = str(state)
     value = 0
     for c in s:
@@ -30,41 +31,41 @@ def successors(state: Board, color : str): #consulta a lista de possiveis movime
     return succ_list
 
 
-def max_val(state : Board, alfa: int, beta: int, color : str): #obtem a lista de possiveis estados de tabuleiro e retorna a avaliacao otimista
+def max_val(state : Board, alfa: int, beta: int, depth: int, color : str): #obtem a lista de possiveis estados de tabuleiro e retorna a avaliacao otimista
     node_value = MIN_SCORE #assumimos que a posicao tem pior valor possivel
     succ_list = successors(state,color)
-    if not succ_list:
+    if not succ_list or depth == 0: #avalia nos-folha
         return eval_func(state, color)
         
     for s_state in succ_list:
-        node_value = max(node_value, min_val(s_state, alfa, beta, color))
+        node_value = max(node_value, min_val(s_state, alfa, beta, depth - 1, color))
         alfa = max(alfa, node_value)
         if alfa >= beta: break #o oponente tem conhecidamente uma jogada melhor
         
     return node_value
 
 
-def min_val(state : Board, alfa: int, beta: int, color : str): #obtem a lista de possiveis estados de tabuleiro e retorna a avaliacao pessimista (otimiza para o oponente)
+def min_val(state : Board, alfa: int, beta: int, depth: int, color : str): #obtem a lista de possiveis estados de tabuleiro e retorna a avaliacao pessimista (otimiza para o oponente)
     node_value = MAX_SCORE #assumimos que a posicao tem melhor valor possivel
     succ_list = successors(state,color)
-    if not succ_list:
+    if not succ_list or depth == 0: #avalia nos-folha
         return eval_func(state, color)
         
     for s_state in succ_list:
-        node_value = min(node_value, max_val(s_state, alfa, beta, color))
+        node_value = min(node_value, max_val(s_state, alfa, beta, depth - 1, color))
         beta = min(beta, node_value)
         if beta <= alfa: break
         
     return node_value
 
 
-def alpha_beta_pruning(state : Board, color: str): #obtem a lista de possiveis estados do tabuleiro e retorna o melhor avaliado
+def alpha_beta_pruning(state : Board, depth: int, color: str): #obtem a lista de possiveis estados do tabuleiro e retorna o melhor avaliado
 
     succ_list = successors(state, color)
     if not succ_list:
         return (-1,-1)
     moves_dict = dict(zip(succ_list, state.legal_moves(color)))
-    best_succ = max(succ_list, key= lambda x: min_val(x, MIN_SCORE, MAX_SCORE, color))
+    best_succ = max(succ_list, key= lambda x: min_val(x, MIN_SCORE, MAX_SCORE, depth - 1, color))
     return moves_dict[best_succ]
     
 def make_move(the_board : Board, color : str):
@@ -74,7 +75,7 @@ def make_move(the_board : Board, color : str):
 	:param color: a character indicating the color to make the move ('B' or 'W')
 	:return: (int, int) tuple with x, y indexes of the move (remember: 0 is the first row/column)
 	"""
-	return alpha_beta_pruning(the_board,color)
+	return alpha_beta_pruning(the_board, DEPTH, color)
     
 
 
